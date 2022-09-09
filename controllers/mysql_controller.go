ae1597eee990aaa233e5208e47e2b4530585923e
zhaoxiaodong.007 <zhaoxiaodong.007@bytedance.com>
2022-09-09 17:38:06 +0800

    controller实现

--- controllers/mysql_controller.go
+++ controllers/mysql_controller.go
@@ -18,6 +18,14 @@ package controllers
 
 import (
 	"context"
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/utils/pointer"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -49,9 +57,127 @@ type MySQLReconciler struct {
 func (r *MySQLReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
-
 	logger.Info("reconcile", "req", req.String())
+
+	mysql := &k8sv1.MySQL{}
+	if err := r.Get(ctx, req.NamespacedName, mysql); err != nil {
+		logger.Error(err, "cannot get mysql object")
+		return ctrl.Result{}, client.IgnoreNotFound(err) // 忽略没有找到的错误，用于删除的情况
+	}
+
+	// 删除中
+	if !mysql.DeletionTimestamp.IsZero() {
+		logger.Info("deleting MySQL" + mysql.Name)
+		return ctrl.Result{}, nil
+	}
+
+	statefulSet := &appsv1.StatefulSet{}
+
+	objectKey := client.ObjectKey{
+		Name:      mysql.Name,
+		Namespace: mysql.Namespace,
+	}
+	if err := r.Get(ctx, objectKey, statefulSet); err != nil && errors.IsNotFound(err) {
+		// 不存在，新建statefulSet
+		ownerRef := []metav1.OwnerReference{
+			{
+				APIVersion:         mysql.APIVersion,
+				Kind:               mysql.Kind,
+				Name:               mysql.Name,
+				Controller:         pointer.BoolPtr(true),
+				BlockOwnerDeletion: pointer.BoolPtr(true),
+				UID:                mysql.UID,
+			},
+		}
+		statefulSet = &appsv1.StatefulSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:            mysql.Name,
+				Namespace:       mysql.Namespace,
+				OwnerReferences: ownerRef,
+			},
+			Spec: appsv1.StatefulSetSpec{
+				ServiceName: "mysql-svc", // TODO: 只走了controller流程，没有实际为StatfulSet创建服务
+				Replicas:    mysql.Spec.Replicas,
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": mysql.Name,
+					},
+				},
+				Template: apiv1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app": mysql.Name,
+						},
+					},
+					Spec: apiv1.PodSpec{
+						TerminationGracePeriodSeconds: pointer.Int64Ptr(10),
+						Containers: []apiv1.Container{
+							{
+								Name:  "mysql",
+								Image: "mysql:" + mysql.Spec.Version,
+								Ports: []apiv1.ContainerPort{
+									{
+										ContainerPort: 3306,
+									},
+								},
+								VolumeMounts: []apiv1.VolumeMount{
+									{
+										Name:      "mysql-store",
+										MountPath: "/var/lib/mysql",
+									},
+								},
+								Env: []apiv1.EnvVar{
+									{
+										// TODO: 只为了描述问题，设置密码为空
+										Name:  "MYSQL_ALLOW_EMPTY_PASSWORD",
+										Value: "true",
+									},
+								},
+							},
+						},
+					},
+				},
+				VolumeClaimTemplates: []apiv1.PersistentVolumeClaim{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "mysql-store",
+						},
+						Spec: apiv1.PersistentVolumeClaimSpec{
+							AccessModes: []apiv1.PersistentVolumeAccessMode{"ReadWriteOnce"},
+							Resources: apiv1.ResourceRequirements{
+								Requests: apiv1.ResourceList{
+									"storage": resource.MustParse("1Gi"),
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		if err := r.Create(ctx, statefulSet); err != nil {
+			logger.Error(err, "create MySQL deployment failed")
+			return ctrl.Result{}, err
+		}
+		logger.Info("create MySQL deployment success: " + statefulSet.GetObjectMeta().GetName())
+
+	} else if err == nil {
+		// 获取到，更新statefulSet
+		retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			statefulSet.Spec.Replicas = mysql.Spec.Replicas
+			statefulSet.Spec.Template.Spec.Containers[0].Image = "mysql:" + mysql.Spec.Version
+			updateErr := r.Update(ctx, statefulSet)
+			return updateErr
+		})
+		if retryErr != nil {
+			logger.Error(retryErr, "update MySQL deployment failed")
+			return ctrl.Result{}, retryErr
+		}
+		logger.Info("update MySQL deployment success: " + statefulSet.GetObjectMeta().GetName())
+
+	} else {
+		logger.Error(err, "cannot get MySQL deployment")
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -60,5 +186,6 @@ func (r *MySQLReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 func (r *MySQLReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&k8sv1.MySQL{}).
+		Owns(&appsv1.StatefulSet{}). // 用来处理删除时的联动删除statefulset
 		Complete(r)
 }
